642bfb904fbf7ab14f4c83da4b35fa963148a019
uruly <rk.uru007@gmail.com>
2019-04-23 15:10:27 +0900

    :rocket: Add progressbar animation

--- SmoothProgress/ViewController.swift
+++ SmoothProgress/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerProgressView: UIProgressView!
+    @IBOutlet weak var animateProgressView: UIProgressView!
+    var timer: Timer?
+    var currentProgress: Float = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timerProgressView.progress = 0
+        animateProgressView.progress = 0
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(changeProgress(_:)), userInfo: nil, repeats: true)
+        UIView.animate(withDuration: 10.0, delay: 0, options: .curveLinear, animations: {
+            self.animateProgressView.setProgress(1, animated: true)
+        }) { (_) in
+            print("おわり")
+        }
+    }
+
+    @objc func changeProgress(_ timer: Timer) {
+        timerProgressView.setProgress(currentProgress / (10 * 10 - 1), animated: true)
+        currentProgress += 1
+        print("よばれ")
     }
 
 
